620bce5e0ec44713b11e834a8c47b0da01e32c3b
basitali97 <56214943+basitali97@users.noreply.github.com>
2019-11-01 16:49:04 +0530

    Update posttest.js

--- experiment/posttest.js
+++ experiment/posttest.js
@@ -1,13 +1,14 @@
 function validate3(){
-    var radio4=document.getElementById("rb11").checked;
-    var radio5=document.getElementById("rb12").checked;
-    var radio6=document.getElementById("rb13").checked;
-    if((radio4=="") && (radio5=="") && (radio6==""))
+    var radio31=document.getElementById("rb31").checked;
+    var radio32=document.getElementById("rb32").checked;
+    var radio33=document.getElementById("rb33").checked;
+    var radio34=document.getElementById("rb34").checked;
+    if((radio31=="") && (radio32=="") && (radio33=="") && (radio34==""))
     {
         alert("please select atleast one!!");
         return false;
     }
-    else if(radio4!="")
+    else if(radio32!="")
     {
     {
         alert("correct answer");
@@ -22,15 +23,16 @@ function validate3(){
 }
 
 function validate1(){
-    var radio7=document.getElementById("rb21").checked;
-    var radio8=document.getElementById("rb22").checked;
-    var radio9=document.getElementById("rb23").checked;
-    if((radio7=="") && (radio8=="") && (radio9==""))
+    var radio41=document.getElementById("rb41").checked;
+    var radio42=document.getElementById("rb42").checked;
+    var radio43=document.getElementById("rb43").checked;
+    var radio44=document.getElementById("rb44").checked;
+    if((radio41=="") && (radio42=="") && (radio43=="") && (radio44==""))
     {
         alert("please select atleast one!!");
         return false;
     }
-    else if(radio7!="")
+    else if(radio44!="")
     {
     {
         alert("correct answer");
@@ -45,15 +47,16 @@ function validate1(){
 }
 
 function validate2(){
-    var radio11=document.getElementById("rb31").checked;
-    var radio12=document.getElementById("rb32").checked;
-    var radio13=document.getElementById("rb33").checked;
-    if((radio11=="") && (radio12=="") && (radio13==""))
+    var radio51=document.getElementById("rb51").checked;
+    var radio52=document.getElementById("rb52").checked;
+    var radio53=document.getElementById("rb53").checked;
+    var radio54=document.getElementById("rb54").checked;
+    if((radio51=="") && (radio52=="") && (radio53=="") && (radio54==""))
     {
         alert("please select atleast one!!");
         return false;
     }
-    else if(radio13!="")
+    else if(radio53!="")
     {
     {
         alert("correct answer");
