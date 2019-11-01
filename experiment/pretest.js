2ba44940634b461a0863deaa9ff2f0fce84915cf
basitali97 <56214943+basitali97@users.noreply.github.com>
2019-11-01 16:40:39 +0530

    Update pretest.js

--- experiment/pretest.js
+++ experiment/pretest.js
@@ -2,16 +2,17 @@ function validate(x){
     var radio1=document.getElementById("rb1").checked;
     var radio2=document.getElementById("rb2").checked;
     var radio3=document.getElementById("rb3").checked;
-    if((radio1=="") && (radio2=="") && (radio3==""))
+    var ratio4=document.getElementById("rb4").checked;
+    if((radio1=="") && (radio2=="") && (radio3=="") && (radio4==""))
     {
         alert("please select atleast one!!");
         return false;
     }
-    else if(radio2!="")
+    else if(radio1!="")
     {
     {
         alert("correct answer");
-    }    
+    }
         validate.location.href="simulation/simulator.html";
     }
 
@@ -20,4 +21,49 @@ function validate(x){
     }
 
 }
+function validate9(x){
+    var radio11=document.getElementById("rb11").checked;
+    var radio12=document.getElementById("rb12").checked;
+    var radio13=document.getElementById("rb13").checked;
+    var ratio14=document.getElementById("rb14").checked;
+    if((radio11=="") && (radio12=="") && (radio13=="") && (radio14==""))
+    {
+        alert("please select atleast one!!");
+        return false;
+    }
+    else if(radio3!="")
+    {
+    {
+        alert("correct answer");
+    }
+        validate.location.href="simulation/simulator.html";
+    }
 
+    else{
+        alert("wrong input");
+    }
+
+}
+function validate8(x){
+    var radio21=document.getElementById("rb21").checked;
+    var radio22=document.getElementById("rb22").checked;
+    var radio23=document.getElementById("rb23").checked;
+    var ratio24=document.getElementById("rb24").checked;
+    if((radio21=="") && (radio22=="") && (radio23=="") && (radio24==""))
+    {
+        alert("please select atleast one!!");
+        return false;
+    }
+    else if(radio4!="")
+    {
+    {
+        alert("correct answer");
+    }
+        validate.location.href="simulation/simulator.html";
+    }
+
+    else{
+        alert("wrong input");
+    }
+
+}
